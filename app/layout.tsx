f05cb5aaee2b5f9e5fc71cbba76d7be59b3a12fd
utopiaexpress123 <basepaxton@gmail.com>
2024-12-29 00:12:52 +0100

    ffgf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider,SignedIn, SignedOut, SignInButton, UserButton  } from '@clerk/nextjs'
+
 
 export const metadata = {
   metadataBase: new URL("https://natural-language-postgres.vercel.app"),
@@ -16,12 +18,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${GeistMono.className} ${GeistSans.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${GeistMono.className} ${GeistSans.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="min-h-screen  relative">
+                <div className="absolute right-4 top-4">
+                      <SignedIn>
+                        {/* Mount the UserButton component */}
+                        <UserButton />
+                      </SignedIn>
+                      <SignedOut>
+                        {/* Signed out users get sign in button */}
+                        <SignInButton />
+                      </SignedOut>
+                </div>
+              <main className="flex-1">
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
